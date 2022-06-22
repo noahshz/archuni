9a69be533d1cfb0b4e27fe45466abdbd6195b39c
Noah <n.scholziii@gmail.com>
2022-06-22 10:23:47 +0200

    added var2

--- archuni/main.py
+++ archuni/main.py
@@ -11,10 +11,10 @@ con = {
     'ip_address' : '10.62.255.50',
     'namespace' : 'archuni',
     'node' : 'robby',
-    'folder' : 'temp_sensor',
-    'variable' : {'key' : 'temperatur', 'value' : '0.0'}
+    'folder' : 'board_sensor',
+    'variable1' : 'temperature',
+    'variable2' : 'humidity'
 }
-
 
 srv = OPCUAServer()
 srv.loadConfig(con)
@@ -25,10 +25,14 @@ try:
     run = 0
     while True:
         temp = sensor.getTemp()
-        #push sensor daten
-        srv.push(temp)
-        #temperaturdaten
-        print(f"Run: {str(run)} Temp: {temp}")
+        humidity = sensor.getHumidity()
+
+        #push temperature
+        srv.push('var1', temp)
+        #push humidity
+        srv.push('var2', humidity)
+        #debug
+        print(f"Run: {str(run)} Temp: {temp} Humidty: {humidity}")
         run = run + 1
 
         time.sleep(1)
--- archuni/server.py
+++ archuni/server.py
@@ -6,8 +6,9 @@ class OPCUAServer:
         'ip_address' : '127.0.0.1',
         'namespace' : 'archuni',
         'node' : 'robby',
-        'folder' : 'temp_sensor',
-        'variable' : {'key' : 'temperatur', 'value' : '0.0'}
+        'folder' : 'board_sensor',
+        'variable1' : 'temperature',
+        'variable2' : 'humidity'
     }
 
     def __init__(self) -> None:
@@ -36,17 +37,21 @@ class OPCUAServer:
         #create folder for object (node)
         self._folder = self._object.add_folder(self._namespace, self._config['folder'])
 
-        #create opcua variable for sensor
-        self._variable = self._folder.add_variable(self._namespace, self._config['variable']['key'],self._config['variable']['value'])
-        self._variable.set_writable()
+        #create opcua variable for sensor temp and humidty
+        # todo
+        self._var1 = self._folder.add_variable(self._namespace, self._config['variable1'],'0')
+        self._var2 = self._folder.add_variable(self._namespace, self._config['variable2'],'0')
+        # self._var1.set_writable()
+        # self._var2.set_writable()
+        
+
+        # self._variable = self._folder.add_variable(self._namespace, self._config['variable']['key'],self._config['variable']['value'])
+        # self._variable.set_writable()
 
         print("-" * 100)
         print('Prepared server with config: ')
         for item in self._config:
-            if item == 'variable':
-                print('\t' + item + ' : ' + self._config[item]['key'] + " => " + self._config[item]['value'])
-            else:
-                print("\t" + item + " : " + self._config[item])
+            print("\t" + item + " : " + self._config[item])
         print("-" * 100)
         pass
     def start(self) -> bool:
@@ -63,11 +68,14 @@ class OPCUAServer:
         except:
             print("[Error] : Verbindungsabbruch nicht möglich.")
             return False
-    def push(self, value: float) -> None:
-        self._variable.set_value(value)
+    def push(self, var: str, value: float) -> None:
+        if var == "var1":
+            self._var1.set_value(value)
+        if var == "var2":
+            self._var2.set_value(value)
         pass
     def loadConfig(self, config: dict) -> bool:
-        if 'ip_address' in config and 'namespace' in config and 'node' in config and 'folder' in config and 'variable' in config:
+        if 'ip_address' in config and 'namespace' in config and 'node' in config and 'folder' in config and 'variable1' in config and 'variable2' in config:
             self._config = config
             self.__init__()
             return True
